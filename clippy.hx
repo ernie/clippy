b80b8c07788bfc8a3db98cb547891eb44956168c
Ernie Miller <ernie@erniemiller.org>
2012-12-04 11:55:28 -0500

    Use callback (w/optional param) to get text

--- clippy.hx
+++ clippy.hx
@@ -4,51 +4,63 @@ import flash.display.SimpleButton;
 import flash.text.TextField;
 import flash.text.TextFieldAutoSize;
 import flash.text.TextFormat;
+import flash.external.ExternalInterface;
+
+class ButtonUp extends MovieClip { public function new() { super(); } }
+class ButtonOver extends MovieClip { public function new() { super(); } }
+class ButtonDown extends MovieClip { public function new() { super(); } }
 
 class Clippy {
   // Main
   static function main() {
-    var text:String = flash.Lib.current.loaderInfo.parameters.text;
-    
+    var callBack:String = flash.Lib.current.loaderInfo.parameters.callBack;
+    var parameter:String = flash.Lib.current.loaderInfo.parameters.parameter;
+
     // label
-    
+
     var label:TextField = new TextField();
     var format:TextFormat = new TextFormat("Arial", 10);
-    
+
     label.text = "copy to clipboard";
     label.setTextFormat(format);
     label.textColor = 0x888888;
     label.selectable = false;
     label.x = 15;
     label.visible = false;
-    
+
     flash.Lib.current.addChild(label);
-    
+
     // button
-    
+
     var button:SimpleButton = new SimpleButton();
     button.useHandCursor = true;
-    button.upState = flash.Lib.attach("button_up");
-    button.overState = flash.Lib.attach("button_over");
-    button.downState = flash.Lib.attach("button_down");
-    button.hitTestState = flash.Lib.attach("button_down");
-    
+    button.upState = flash.Lib.attach("ButtonUp");
+    button.overState = flash.Lib.attach("ButtonOver");
+    button.downState = flash.Lib.attach("ButtonDown");
+    button.hitTestState = flash.Lib.attach("ButtonDown");
+
     button.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) {
+      var text:String;
+      if (parameter != null) {
+        text = ExternalInterface.call(callBack, parameter);
+      } else {
+        text = ExternalInterface.call(callBack);
+      }
       flash.system.System.setClipboard(text);
       label.text = "copied!";
       label.setTextFormat(format);
     });
-    
+
     button.addEventListener(MouseEvent.MOUSE_OVER, function(e:MouseEvent) {
       label.visible = true;
     });
-    
+
     button.addEventListener(MouseEvent.MOUSE_OUT, function(e:MouseEvent) {
       label.visible = false;
       label.text = "copy to clipboard";
       label.setTextFormat(format);
     });
-    
+
     flash.Lib.current.addChild(button);
   }
 }
